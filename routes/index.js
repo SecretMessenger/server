086eb29d2e083390463621ebcac147383e1b7c07
Angela Vanessa <hansavanessa@gmail.com>
2019-12-05 18:29:51 +0700

    multer and fs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const fs = require('fs')
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './images')
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+      cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+const upload = multer({storage: storage});
 
 router.get('/', function(req, res, next) {
     res.send('Hello world!')
+})
+
+router.post('/encrypt', upload.single('image'), function(req, res, next) {
+    // sudah write file ke folder images
+
+    // lanjut encrypt disini
+
+    // ini untuk delete file yg di folder images
+    fs.unlinkSync(req.file.path)
+
+    // upload ke gcs dan res send url
 })
 
 module.exports = router;
