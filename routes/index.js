925e32dd6078e8639081278ee5e2c71bdc8d8c9e
risanto <potallegta@gmail.com>
2019-12-05 18:25:15 +0700

    route encode and decode in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
+const stego = require("stegosaurus");
 const router = require('express').Router();
 
-router.get('/', function(req, res, next) {
-    res.send('Hello world!')
+router.get('/', function (req, res, next) {
+  res.send('Hello world!')
+})
+
+var original_png = "";		// Image you want to encode with a message, must be png.
+var generated_png = "";		// The resulting file with image that can be encoded.
+var message_string = "";  // The encoded message.
+
+router.post('/encode', function (req, res, next) {
+  stego.encodeString(original_png, generated_png, message_string, function (err) {
+    if (err) { throw err; }
+    console.log("Wrote png to: ", generated_png)
+    res.status(200).json({
+      message: 'Encoded your message to this image.',
+      image: generated_png,
+      message_length: message_string.length // use this when you want to decode the image 
+    })
+  })
+})
+
+router.post('/decode', function (req, res, next) {
+  const { message_length } = req.body
+
+  stego.decode(generated_png, Number(message_length), function (payload) {
+    console.log("Decoded message: ", payload);
+    res.status(200).json({
+      message: 'Decoded message in your image.',
+      image: generated_png,
+      decoded_message: payload
+    })
+  })
 })
 
 module.exports = router;
---
+++ test.js
@@ -0,0 +1,65 @@
+/**
+ * @file test.js
+ * @brief Test driver for Stegger.js
+ * @author Joel Haowen TONG
+ * @version 0.1.1
+ * @date 2013-07-19
+ */
+
+
+/** This test module embeds the image with a UTF-8 string (Korean and English),
+ * using 2-layer encryption.  AES-256 cipher is first used, with a secret key.
+ * Thereafter, the resultant crypted string is salted with the passphrase.
+ *
+ * Note: stegger.encrypt() and decrypt() return promises, which must then be listened for a result.
+ *
+ */
+
+var stegger = require('stegger')
+    , Sequence = require('futures').sequence;
+
+
+Sequence()
+.then(function(next) {
+    console.log('');
+    console.log('--Testing ENCRYPT--')
+    stegger.encrypt({
+        inFile     : 'hjbjk9h2.jpg',
+        outFile    : 'output.jpg',
+        msg        : 'test-rahasia',
+        key        : 'bla bla',
+        method     : 'aes-256-cbc',
+        passphrase : 'blue dog'
+    })
+
+    .when(function(err, data) {
+        console.log('[OUTPUT] image path: ' + data + '\n');
+        next(data);
+    
+    });
+
+})
+
+
+.then(function(next, data) {
+    console.log('--Testing DECRYPT--')
+    stegger.decrypt({
+        inFile     : data,
+        outFile    : 'decrypted.txt',
+        key        : 'bla bla',
+        method     : 'aes-256-cbc',
+        passphrase : 'blue dog'
+
+    })
+
+    .when(function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        console.log('[OUTPUT] Decrypted message: ' + data + '\n');
+
+    });
+
+});
+
