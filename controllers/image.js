965550ef5c9e16dad2a1c61ef906a6d8fdac8663
Angela Vanessa <hansavanessa@gmail.com>
2019-12-06 12:14:26 +0700

    decode via url working

--- controllers/image.js
+++ controllers/image.js
@@ -1,11 +1,11 @@
 const stego = require("stegosaurus");
 const fs = require('fs');
+const axios = require('axios')
 const uploadImage = require('../helpers/uploadImage');
 
 class ImageController {
     static encode(req, res, next) {
-        // const { message_string } = req.body;
-        var message_string = "asd";  // The encoded message.
+        const { message_string } = req.body;
         let generated_png = req.file.destination + '/encoded/' + req.file.filename;
         stego.encodeString(req.file.path, generated_png, message_string, function (err) {
           if (err) { next(err) }
@@ -17,7 +17,10 @@ class ImageController {
                 uploadImage({ file })
                 .then(url => {
                     fs.unlinkSync(generated_png);
-                    res.status(200).json({url});
+                    res.status(200).json({
+                        url,
+                        password: message_string.length
+                    });
                 })
             } catch (error) {
                 next(error)
@@ -36,6 +39,40 @@ class ImageController {
             decoded_message: payload
           })
         })
+    }
+
+    static decodeUrl(req, res, next) {
+        const { url, message_length } = req.body;
+        
+        let filename = url.split('/')
+        filename = filename[filename.length - 1];
+        let filepath = `./images/writeStream/${filename}`;
+        const writer = fs.createWriteStream(filepath)
+      
+        axios({
+          url,
+          method: 'GET',
+          responseType: 'stream'
+        })
+        .then(({data}) => {
+            data.pipe(writer);
+            return new Promise((resolve, reject) => {
+                writer.on('finish', resolve)
+                writer.on('error', reject)
+            })
+        })
+        .then( _=> {
+            
+            stego.decode(filepath, Number(message_length), function (payload) {
+                fs.unlinkSync(filepath);
+                res.status(200).json({
+                  message: 'Decoded message in your image.',
+                  image: filepath,
+                  decoded_message: payload
+                })
+            })
+        })
+        .catch(next)
     }
 }
 
